a9582ac81617a486147b49e411b94f716c324231
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-10-05 11:08:56 -0300

    req2

--- src/routes/talkerRoute.js
+++ src/routes/talkerRoute.js
@@ -14,4 +14,17 @@ router.get('/', async (req, res) => {
         res.status(200).json(talkerData);
     }
 });
+
+  router.get('/:id', async (req, res) => {
+    const talkerData = JSON.parse(await fs.readFile(talkerPath, 'utf8'));
+    const talkerId = talkerData.find(({ id }) => id === Number(req.params.id));
+    if (!talkerId) {
+    res.status(404).json({
+        message: 'Pessoa palestrante não encontrada',
+      });
+} else {
+    res.status(200).json(talkerId);
+}
+  });
+
 module.exports = router;
